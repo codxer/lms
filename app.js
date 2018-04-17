81c333ee56bf51ffe831fa6cc208d1d53bc3c0cb
Abdullah <seocrawlerz@gmail.com>
2018-04-17 11:05:31 +0500

    This is my master commit ...

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('This is a console message');
+var jsonForm = [{
+    type: 'text',
+    name: 'inputField1',
+    required: true,
+    value: 'This is the input field',
+    placeholder: 'This is the input placeholder'
+}];
+
+function generateForm(){
+    console.log('Calling...');
+    var element = document.getElementById('form');
+    jsonForm.forEach(function(item){
+        var formItem = '';
+        switch (item.type) {
+            case 'text':
+                formItem = document.createElement('input');
+                formItem.type = 'text';
+                formItem.name = item.name;
+                formItem.value = item.value;
+                formItem.placeholder = item.placeholder;
+                formItem.required = item.required;
+                break;
+            case 'drop-down':
+                break;
+            case 'text-area':
+                break;
+            case 'check-box':
+                break;
+            case 'radio':
+                break;
+        }
+        element.appendChild(formItem);
+    });
+
+};
